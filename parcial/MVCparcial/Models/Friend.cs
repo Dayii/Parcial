2ce710fae2f5ad4d85cb8c37aa871c0e55d9ea78
dianateranargandona@gmail.com <dianateranargandona@gmail.com>
2019-04-06 10:39:11 -0300

    Commit modelo Friend de proyecto mvc

---
+++ parcial/MVCparcial/Models/Friend.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCparcial.Models
+{
+    public enum TipoAmigo
+    {
+        Conocido,
+        Compañero,
+        Colega,
+        AmigoDeInfancia,
+        Pariente
+
+    }
+
+    public class Friend
+    {
+        [Key]
+        public int FriendId { get; set; }
+
+        [Required]
+        public string  Name { get; set; }
+
+       
+        public string Nickname { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        [Required]
+        public TipoAmigo Type { get; set; }
+    }
+}
