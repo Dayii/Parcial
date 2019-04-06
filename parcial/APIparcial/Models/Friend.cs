07eb321199715f4842ce52f3e4410647b6ecc212
dianateranargandona@gmail.com <dianateranargandona@gmail.com>
2019-04-06 10:45:00 -0300

    Commit modelo web api

---
+++ parcial/APIparcial/Models/Friend.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIparcial.Models
+{
+    public enum TipoAmigo
+    {
+        Conocido,
+        Compañero,
+        Colega,
+        AmigoDeInfancia,
+        Pariente
+
+    }
+
+    public class Friend
+    {
+        [Key]
+        public int FriendId { get; set; }
+
+        [Required]
+        public string  Name { get; set; }
+
+       
+        public string Nickname { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        [Required]
+        public TipoAmigo Type { get; set; }
+    }
+}
